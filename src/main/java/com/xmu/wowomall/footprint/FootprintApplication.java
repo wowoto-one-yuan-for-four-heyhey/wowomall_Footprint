c7d2829c2f5aabfa25837416891952f8703a0db4
Tens <tens444@163.com>
2019-12-11 17:11:20 +0800

    修订1

--- src/main/java/com/xmu/wowomall/footprint/FootprintApplication.java
+++ src/main/java/com/xmu/wowomall/footprint/FootprintApplication.java
@@ -3,9 +3,11 @@ package com.xmu.wowomall.footprint;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @MapperScan("com.xmu.wowomall.footprint.mapper")
+@EnableEurekaClient
 public class FootprintApplication {
 
     public static void main(String[] args) {
