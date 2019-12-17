845c8fabf3aac6388a7e32f2a4aa1eb27dd4748a
medalwill <42199368+medalwill@users.noreply.github.com>
2019-12-17 21:20:59 +0800

    修订1

--- src/main/java/com/xmu/wowomall/footprint/service/RemoteGoodsService.java
+++ src/main/java/com/xmu/wowomall/footprint/service/RemoteGoodsService.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 @Service
-@FeignClient("GoodsInfo")
+@FeignClient("goodsInfoService")
 public interface RemoteGoodsService {
 
     /**
